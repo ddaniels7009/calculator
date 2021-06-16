79892d36abe7e5ddc399eb1aeed0085e56a1bf79
david daniels <ddaniels7009@gmail.com>
2021-06-16 15:19:00 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const displayContainer = document.getElementById('screen');
+
+
+
+const button1 = document.getElementById('1');
+const button2 = document.getElementById('2');
+const button3 = document.getElementById('3');
+const button4 = document.getElementById('4');
+const button5 = document.getElementById('5');
+const button6 = document.getElementById('6');
+const button7 = document.getElementById('7');
+const button8 = document.getElementById('8');
+const button9 = document.getElementById('9');
+
+
+
+let displayValue = 0;
+let operand1 = 0;
+let operand2 = 0;
+
+display(displayValue);
+
+function operate(operand1, operand2, operator){
+
+
+
+}
+
+function display(displayValue){
+
+
+}
+
+
+
+function sum(operand1, operand2){
+
+    let sum = 0;
+
+    sum = operand1 + operand2;
+
+    return sum;
+
+}
+
+
+displayContainer.innerText = (displayValue)
+console.log(displayValue)
