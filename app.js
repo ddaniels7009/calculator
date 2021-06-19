df608836e1db563aadeeff8f497131f768ad1ed5
david daniels <ddaniels7009@gmail.com>
2021-06-19 10:59:06 -0600

    Added functionality to allow user to input more than one operation at a time.

--- app.js
+++ app.js
@@ -19,128 +19,132 @@ const divide = document.getElementById('divide');
 const multiply = document.getElementById('multiply');
 const equals = document.getElementById('equals');
 
-button0.addEventListener('click', function () { display("0"); gatherValues("0") });
-button1.addEventListener('click', function () { display("1"); gatherValues("1") });
-button2.addEventListener('click', function () { display("2"); gatherValues("2") });
-button3.addEventListener('click', function () { display("3"); gatherValues("3") });
-button4.addEventListener('click', function () { display("4"); gatherValues("4") });
-button5.addEventListener('click', function () { display("5"); gatherValues("5") });
-button6.addEventListener('click', function () { display("6"); gatherValues("6") });
-button7.addEventListener('click', function () { display("7"); gatherValues("7") });
-button8.addEventListener('click', function () { display("8"); gatherValues("8") });
-button9.addEventListener('click', function () { display("9"); gatherValues("9") });
+button0.addEventListener('click', function () { display("0"); setOperand("0") });
+button1.addEventListener('click', function () { display("1"); setOperand("1") });
+button2.addEventListener('click', function () { display("2"); setOperand("2") });
+button3.addEventListener('click', function () { display("3"); setOperand("3") });
+button4.addEventListener('click', function () { display("4"); setOperand("4") });
+button5.addEventListener('click', function () { display("5"); setOperand("5") });
+button6.addEventListener('click', function () { display("6"); setOperand("6") });
+button7.addEventListener('click', function () { display("7"); setOperand("7") });
+button8.addEventListener('click', function () { display("8"); setOperand("8") });
+button9.addEventListener('click', function () { display("9"); setOperand("9") });
 clear.addEventListener('click', function () { clearAll(); });
-plus.addEventListener('click', function () { display(" + "); gatherValues("+") });
-minus.addEventListener('click', function () { display(" - "); gatherValues("-") });
-divide.addEventListener('click', function () { display(" / ");; gatherValues("/") });
-multiply.addEventListener('click', function () { display(" x "); gatherValues("x") });
-equals.addEventListener('click', function () { display(" = "); gatherValues("=") });
+plus.addEventListener('click', function () { display(" + "); setOperand("+") });
+minus.addEventListener('click', function () { display(" - "); setOperand("-") });
+divide.addEventListener('click', function () { display(" / ");; setOperand("/") });
+multiply.addEventListener('click', function () { display(" x "); setOperand("x") });
+equals.addEventListener('click', function () { display(" = "); setOperand("=") });
+
 
 let displayValue = 0;
+
 let operand1 = "";
 let operand2 = "";
-let operator = "";
-let operator2 = "";
-let runningTotal = 0;
+let operators = [];
+let result = 0;
+let count = 0;
 let firstTime = true;
+
 let operatorCount = 0;
-let holdMe = 0;
+let operatorPresent = false;
 
-display(displayValue);
 
-function gatherValues(input) {
-
-    // Check if input is an operator. If it is, set the operator and add 1 to the operator count
-    if (input == "+" || input == "-" || input == "/" || input == "x") {
-       
-        operator = input;
-        operatorCount++;
-        
-        
-        //console.log("Operator : " + operator);
-    }
+function setOperand(input){
     
- 
+// set the initial operand
+if(firstTime==true && Number.isInteger(parseInt(input))) {
+    operand1 += input;
+    //console.log("Operand 1: " + operand1)
+}
 
-    // check if operator is the same as last time. **********************
+
+// set the initital operator
+if(input == "+" && firstTime == true){
+    operators[count] = input;
+    //console.log("Operator: " + operators[count]);
+    firstTime=false;
+    count++;
+    operatorCount++;
+    operatorPresent = true;
+}
+else if (input == "+" && firstTime == false) {
+    operators[count] = input;
+    count++;
+    operatorCount++;
+}
+
+else if(operatorCount > 0 && input != "+" && input != "=" && operand1 != "" && Number.isInteger(parseInt(input))){
+    operand2 += input;
+    //console.log("Operand 2: " + operand2)
+}
+else if(operatorPresent==true && operatorCount == 0 && input != "+" && input != "=" && operand1 != "" && Number.isInteger(parseInt(input))){
+    operand2 += input;
+    //console.log("Operand 2: " + operand2)
+}
+
+
+// Operate if user inputs an '=' sign
+if(input == "="){
+    //console.log("Operand 1 Second Round: " + operand1)
+    //console.log("op count-1: " + operators[0] );
+result = operate(operand1, operand2, operators[count-1]);
+operand1 = result;
+displayEquals(result);
+}
+
+// Operate if user puts a second operator
+if(input == "+" && operand2 != "" && operators[count-1] != undefined){// CHANGE BACK TO "2" IF ERRORS!!!!!!
+    console.log("fuck")
+    console.log("operand1::" + operand1)
+    console.log("op coint" + operatorCount)
+    result = operate(operand1, operand2, operators[count-1]);
+    operand1 = result;
     
-
-
-    // Check if the first operand has already been set. If it hasn't, set the operand based on user input until another operator is entererd
-    if (operator == "" && firstTime == true) {
-
-        operand1 += input;
-
-
-    }
-
-    //set the first operand to the running total in order to run the next operation
-    if (firstTime == false) {
-       operand1 = runningTotal;
-        console.log("wtf" + operand1);
-    }
-
-    // Set the second operand as long as the first operand has been set and the input is not an operator
-    if (operator != "" && input != "+" && input != "-" && input != "/" && input != "x" && input != "=") {
-
-        operand2 += input;
-
-    }
-
-
-
-    // Store all calculations prior to hitting equal sign
-    if (operatorCount == 2 || input == "=" && firstTime == true) {
-
-        console.log("Before Operator: " + operator);
-        console.log("Before Op1: " + operand1);
-        console.log("Before Op2: " + operand2);
-
-
-        runningTotal = operate(operand1, operand2, operator);
-        //operand1 = runningTotal;
-
-    }
-
-
-    if (firstTime == false) {
-        operand1 = runningTotal;
-    
-        console.log("wtf" + operand1);
-    }
-
-    if (input == "=") {
-
-        display(runningTotal, "equals");
-    }
+}
 
 
 }
 
-function setO(xxx){
+function clearAll(){
+   
+    displayValue = 0;
 
-}
-
-function clearOperand() {
+    operand1 = "";
     operand2 = "";
-    operatorCount--;
-    firstTime = false;
+    operators = [];
+    result = 0;
+    count = 0;
+    firstTime = true;
+    display("clear")
+    operatorPresent = false;
+    operatorCount = 0;
+
+
+}
+
+function reset(){
+
+    operatorCount = 0;
+    operand2 = "";
+    result = 0;
 
 }
 
 
-function operate(operand1, operand2, operator) {
 
+function operate(operand1, operand2, operators) {
 
-    console.log("After Operator: " + operator);
+    
+    console.log("After Operator: " + operators);
     console.log("After Op1: " + operand1);
     console.log("After Op2: " + operand2);
 
     let x = parseFloat(operand1);
     let y = parseFloat(operand2);
-    let z = operator;
+    let z = operators;
 
-    clearOperand();
+    reset();
 
     if (z == "+") {
 
@@ -161,17 +165,13 @@ function operate(operand1, operand2, operator) {
 }
 
 // Set display value based on user's button presses
-function display(inputValue, equals) {
+function display(inputValue) {
 
     if (inputValue == "clear") {
         displayValue = 0;
         inputValue = 0;
     }
-
     else if (displayValue == 0) {
-        displayValue = inputValue;
-    }
-    else if (equals == "equals") {
         displayValue = inputValue;
     }
     else {
@@ -182,20 +182,10 @@ function display(inputValue, equals) {
     displayContainer.innerText = (displayValue)
 }
 
-function empty() {
-    operand1 = "";
-    operand2 = "";
-    operator = "";
-}
+function displayEquals(inputValue){
 
-// Set display value to 0 if clear button is pressed
-function clearAll() {
-    display(displayValue = 0);
-    operand1 = "";
-    operand2 = "";
-    operator = "";
-    runningTotal = 0;
-    firstTime = true;
+    displayContainer.innerText = (inputValue)
+
 }
 
 // Sum funtion
@@ -219,4 +209,3 @@ function multiplication(operand1, operand2) {
     return operand1 * operand2;
 
 }
-
